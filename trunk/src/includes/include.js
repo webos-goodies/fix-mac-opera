4d5127ec9e87a8729c1294a0cae3b8f7e777517b
sourcewalker@gmail.com <sourcewalker@gmail.com@4c2cc761-7c68-7b68-79ea-33ecbe4d3326>
2011-04-13 03:37:12 +0000

    Handles key repeat correctly.

--- trunk/src/includes/include.js
+++ trunk/src/includes/include.js
@@ -1,10 +1,25 @@
-function onEvent(e) {
-  if(e.metaKey &&
-	 ((65 <= e.keyCode && e.keyCode <= 90) || (97 <= e.keyCode && e.keyCode <= 122))) {
-	e.preventDefault();
-    e.stopPropagation();
+(function() {
+  var repeatFlags = {};
+  function onEvent(e) {
+	if(e.metaKey) {
+      if((65 <= e.keyCode && e.keyCode <= 90) ||
+		 (97 <= e.keyCode && e.keyCode <= 122)) {
+		e.preventDefault();
+		e.stopPropagation();
+		if(e.type == 'keypress') {
+          repeatFlags[e.keyCode & 0x1f] = false;
+		}
+      } else if(e.type == 'keypress' &&
+				1 <= e.keyCode && e.keyCode <= 26) {
+		if(repeatFlags[e.keyCode]) {
+          e.preventDefault();
+          e.stopPropagation();
+		}
+		repeatFlags[e.keyCode] = !repeatFlags[e.keyCode];
+      }
+	}
   }
-}
-document.addEventListener('keypress', onEvent, true);
-document.addEventListener('keydown', onEvent, true);
-document.addEventListener('keyup', onEvent, true);
+  document.addEventListener('keypress', onEvent, true);
+  document.addEventListener('keydown', onEvent, true);
+  document.addEventListener('keyup', onEvent, true);
+})();
