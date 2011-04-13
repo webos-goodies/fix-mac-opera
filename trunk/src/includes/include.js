65f060881edb84cfad5de7be5dd17536aa4bd7c0
sourcewalker@gmail.com <sourcewalker@gmail.com@4c2cc761-7c68-7b68-79ea-33ecbe4d3326>
2011-04-13 07:26:59 +0000

    Do nothin on all platforms except Mac.

--- trunk/src/includes/include.js
+++ trunk/src/includes/include.js
@@ -1,25 +1,29 @@
 (function() {
   var repeatFlags = {};
+
   function onEvent(e) {
-	if(e.metaKey) {
+    if(e.metaKey) {
       if((65 <= e.keyCode && e.keyCode <= 90) ||
-		 (97 <= e.keyCode && e.keyCode <= 122)) {
-		e.preventDefault();
-		e.stopPropagation();
-		if(e.type == 'keypress') {
+         (97 <= e.keyCode && e.keyCode <= 122)) {
+        e.preventDefault();
+        e.stopPropagation();
+        if(e.type == 'keypress') {
           repeatFlags[e.keyCode & 0x1f] = false;
-		}
+        }
       } else if(e.type == 'keypress' &&
-				1 <= e.keyCode && e.keyCode <= 26) {
-		if(repeatFlags[e.keyCode]) {
+                1 <= e.keyCode && e.keyCode <= 26) {
+        if(repeatFlags[e.keyCode]) {
           e.preventDefault();
           e.stopPropagation();
-		}
-		repeatFlags[e.keyCode] = !repeatFlags[e.keyCode];
+        }
+        repeatFlags[e.keyCode] = !repeatFlags[e.keyCode];
       }
-	}
+    }
   }
-  document.addEventListener('keypress', onEvent, true);
-  document.addEventListener('keydown', onEvent, true);
-  document.addEventListener('keyup', onEvent, true);
+
+  if(/^mac/i.test(window.navigator.platform)) {
+    document.addEventListener('keypress', onEvent, true);
+    document.addEventListener('keydown', onEvent, true);
+    document.addEventListener('keyup', onEvent, true);
+  }
 })();
