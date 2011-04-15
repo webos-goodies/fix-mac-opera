f2004e3e30c7cfdf415c3df8e99298f5d1933b26
sourcewalker <sourcewalker@gmail.com>
2011-04-15 11:05:29 +0000

    Ready to publish on the official directory.

--- src/includes/include.js
+++ src/includes/include.js
@@ -6,7 +6,6 @@
       if((65 <= e.keyCode && e.keyCode <= 90) ||
          (97 <= e.keyCode && e.keyCode <= 122)) {
         e.preventDefault();
-        e.stopPropagation();
         if(e.type == 'keypress') {
           repeatFlags[e.keyCode & 0x1f] = false;
         }
@@ -14,7 +13,6 @@
                 1 <= e.keyCode && e.keyCode <= 26) {
         if(repeatFlags[e.keyCode]) {
           e.preventDefault();
-          e.stopPropagation();
         }
         repeatFlags[e.keyCode] = !repeatFlags[e.keyCode];
       }
